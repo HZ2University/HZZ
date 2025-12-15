6f106796e4258186542087214e9e79ab9c59d586
HZ2University <2049682157@qq.com>
2025-12-15 16:16:22 +0800

    Add files via upload

---
+++ NWEW/硬件驱动源码/all.h
@@ -0,0 +1,3 @@
+extern char *sensor(int *temp, int *humi, float *illum);
+extern int LED_Open(int on_off);
+extern int SEmotor(int angle);
---
+++ NWEW/硬件驱动源码/functions.h
@@ -0,0 +1,8 @@
+// functions.h
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void getMultipleValues(int *a, float *b, char *c);
+
+#endif /* FUNCTIONS_H */
